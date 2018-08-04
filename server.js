d483180c3e3e1395d2cc2e5919f3a4aef11b072c
musphilo <fm_bouguesri@esi.dz>
2018-08-04 14:38:07 +0100

    Hello Express server practised the basics of creating a web server with express handling to urls sending back html and JSON

---
+++ server.js
@@ -0,0 +1,29 @@
+/*-------------Node and 3rd party modules-------------*/
+const express = require('express');
+
+/*------------------Personnel Modules-----------------*/
+
+/*-------------------Initialisations------------------*/
+const app = express();
+
+/*------------------------Code------------------------*/
+app.get('/', (req, res)=> {
+    res.send('<h1>Hello Express</h1>');
+    
+});
+
+app.get('/about', (req, res)=> {
+    //res.send('<h1>about page</h1>');
+    res.send({
+        domaine_name : 'express-web-server.net',
+        decription : 'a web app to practise creation of node web servers using Express.JS',
+        author : {
+            user_name : 'BOUGUESRIAdel',
+            first_name : 'Adel',
+            age : '22'
+        },
+        onGithub : true
+    });
+});
+
+app.listen(3000);
